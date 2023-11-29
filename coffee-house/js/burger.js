25172deff0eeec7430c9e84d932c3a4aa957e4bc
Luba Hadunova <luba.godunova2018@gmail.com>
2023-11-29 13:06:19 +0100

    feat: add burger-menu

---
+++ coffee-house/js/burger.js
@@ -0,0 +1,41 @@
+const btnBurger = document.querySelector('.button-burger');
+const header = document.querySelector('.header');
+const header__nav = document.querySelector('.header__nav');
+const headerLinks = document.querySelectorAll('.header__link');
+const headerMenuLink = document.querySelector('.header__menu__link');
+
+btnBurger.addEventListener('click', function(event) {
+    event._isClickOnTneBurgerMenu = true;
+    burger();
+});
+
+headerLinks.forEach(element => {
+    element.addEventListener('click', clickOnTheLink);
+});
+
+headerMenuLink.addEventListener('click', (event) => {
+    event.preventDefault();
+    clickOnTheLink();
+});
+
+function burger() {
+    header.classList.toggle('show');
+    stopScroll();
+}
+
+function stopScroll() {
+    document.body.classList.toggle('show-menu');
+}
+
+function allowScroll() {
+    document.body.classList.remove('show-menu');
+}
+
+function clickOnTheLink() {
+    console.log(1);
+    header.classList.remove('show');
+    allowScroll();
+}
+
+
+
