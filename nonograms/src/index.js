67a79d19eaf8dce24f42556071354df64a932f22
Luba Hadunova <luba.godunova2018@gmail.com>
2024-02-04 18:59:25 +0100

    fix: change height to body and container

--- nonograms/src/index.js
+++ nonograms/src/index.js
@@ -1,3 +1,6 @@
 import "./style.scss";
 import "./js/music";
 import "./js/layout";
+
+const message = `Привет!\nДля тестирования на мобильном устройстве необходимо в DevTools выбрать режим Mobile, а потом перегрузить страницу.\nЕсли есть вопросы или замечания - пиши!\nСпасибо за проверку!`;
+console.log(message);
