f6ec7ad99b8b8e1077e23adde9d982376b1d7ffb
Luba Hadunova <luba.godunova2018@gmail.com>
2024-02-23 23:57:14 +0100

    feat: add feature to filter sources by by pressing button with letter

---
+++ news-api/src/alphabet.ts
@@ -0,0 +1,87 @@
+import './alphabet.css';
+import { Layout } from './components/abstracts/classes';
+
+export class Alphabet extends Layout {
+    public createAlphabet(): void {
+        const main = document.querySelector('main');
+        if (main) {
+            const mainAlphabet = this.createElement('div', 'alphabet');
+            this.addAlphabetFunctions(mainAlphabet);
+            this.createAlphabetBtns(mainAlphabet);
+            main.prepend(mainAlphabet);
+        }
+    }
+
+    private createAlphabetBtns(item: Element): void {
+        const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        for (let i = 0; i < alphabet.length; i += 1) {
+            const btn = this.createElement('button', 'alphabet__btn buttons', alphabet[i]);
+            btn.setAttribute('data-literal', alphabet[i]);
+            item.append(btn);
+        }
+    }
+
+    private addAlphabetFunctions(item: Element): void {
+        item.addEventListener('click', this.filterSources);
+    }
+
+    private filterSources(e: Event): void {
+        const target = e.target as HTMLElement;
+        if (target.classList.contains('alphabet__btn')) {
+            const literal = target.dataset.literal;
+            if (literal) {
+                const checked = target.getAttribute('checked');
+                if (checked && checked === 'true') {
+                    alphabet.showAllSources();
+                    alphabet.removeAttributeChecked();
+                    return;
+                }
+                alphabet.removeAttributeChecked();
+                target.setAttribute('checked', 'true');
+                alphabet.hideSourses(literal);
+            }
+        }
+    }
+
+    private removeAttributeChecked(): void {
+        const btns = [...document.querySelectorAll('.alphabet__btn[checked="true"]')];
+        btns.forEach((item) => item.removeAttribute('checked'));
+    }
+
+    private showAllSources(): void {
+        const sources = [...document.querySelectorAll('.source__item')];
+        sources.filter((item) => item.classList.contains('hide')).map((item) => item.classList.remove('hide'));
+    }
+
+    private filterFunction(item: Element, literal: string): boolean {
+        const sourceTitle = item.getAttribute('data-source-id');
+        if (sourceTitle) {
+            if (sourceTitle[0].toUpperCase() === literal.toUpperCase()) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private hideSourses(literal: string): void {
+        const sources = [...document.querySelectorAll('.source__item')];
+        this.showAllSources();
+        sources.filter((item) => this.filterFunction(item, literal)).map((item) => item.classList.add('hide'));
+        this.clearNews();
+    }
+
+    private clearNews(): void {
+        const news = document.querySelector('.news');
+        if (news) {
+            news.innerHTML = '';
+        }
+    }
+}
+
+const alphabet = new Alphabet();
+
+document.addEventListener('DOMContentLoaded', addAlphabetToLayout);
+
+function addAlphabetToLayout(): void {
+    alphabet.createAlphabet();
+}
--- news-api/src/index.ts
+++ news-api/src/index.ts
@@ -1,6 +1,7 @@
 import App from './components/app/app';
 import './global.css';
 import './burger';
+import './alphabet';
 import './footer';
 
 const app = new App();
