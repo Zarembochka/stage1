b98fdddda2fdc7f2d54a8ff6a8b5f472cc093714
Luba <128534497+Zarembochka@users.noreply.github.com>
2024-03-04 08:58:16 +0100

    feat: install webpack, typescript, eslint, prettier (#54)

---
+++ rss-puzzle/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const baseConfig = {
+    entry: "./src/index.ts",
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(scss|css)$/i,
+                use: [miniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(ico|gif|png|jpg|jpeg|svg)$/i,
+                type: "asset/resource",
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".js", ".ts"],
+    },
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        filename: "index.js",
+        assetModuleFilename: "assets/[name][ext]",
+    },
+    plugins: [
+        new htmlWebpackPlugin({ title: "Rss Puzzle" }),
+        new miniCssExtractPlugin({ filename: "[name].css" }),
+        new CleanWebpackPlugin(),
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === "prod";
+    const envConfig = isProductionMode ? require("./webpack.prod.config") : require("./webpack.dev.config");
+
+    return merge(baseConfig, envConfig);
+};
