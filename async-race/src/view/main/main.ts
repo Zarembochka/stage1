25f309689b0bd9f785e75c52d7233c4281745d2c
Luba Hadunova <luba.godunova2018@gmail.com>
2024-03-21 21:17:14 +0100

    feat: add setting buttons

--- async-race/src/view/main/main.ts
+++ async-race/src/view/main/main.ts
@@ -8,21 +8,56 @@ export class Main extends BaseComponent {
     }
 
     private prepareMain(): void {
-        this.createNewCarField();
+        this.createSettings();
     }
 
-    private createNewCarField(): void {
-        const wrapper = this.createWrapperToField();
+    private createSettings(): void {
+        const wrapper = this.createWrapperToField(["main__settings"]);
+        const formCreate = this.createFormNewCar();
+        const formUpdate = this.createFormUpdateCar();
+        const buttons = this.createSettingButtons();
+        wrapper.append(formCreate, formUpdate, buttons);
+    }
+
+    private createSettingButtons(): Element {
+        const wrapper = this.createWrapperToField(["main__setting__buttons"]);
+        const btnRace = this.createBtn(["btn", "btn__setting", "btn-race"], "Race");
+        const btnReset = this.createBtn(["btn", "btn__setting", "btn-reset"], "Reset");
+        const btnGenerate = this.createBtn(["btn", "btn__setting", "btn-generate"], "Generate cars");
+        wrapper.append(btnRace, btnReset, btnGenerate);
+        return wrapper;
+    }
+
+    private createFormNewCar(): HTMLFormElement {
+        const formCreate = this.createForm("form__create");
         const input = this.createInput(["input", "input__newCar"], InputTYPES.text);
         const colorChoice = this.createInput(["input", "input__newCarColor"], InputTYPES.color);
-        const btn = this.createBtn();
-        wrapper?.append(input, colorChoice, btn);
+        const btn = this.createBtn(["btn", "btn__main", "btn-create"], "Create");
+        formCreate?.append(input, colorChoice, btn);
+        return formCreate;
     }
 
-    private createWrapperToField(): Element {
-        const wrapper = new BaseComponent({ tag: "div", classNames: ["main__newCar"] }).getElement();
+    private createFormUpdateCar(): HTMLFormElement {
+        const formCreate = this.createForm("form__update");
+        const input = this.createInput(["input", "input__updateCar"], InputTYPES.text);
+        const colorChoice = this.createInput(["input", "input__updateCarColor"], InputTYPES.color);
+        const btn = this.createBtn(["btn", "btn__main", "btn-update"], "Update");
+        formCreate?.append(input, colorChoice, btn);
+        return formCreate;
+    }
+
+    private createWrapperToField(classNames: string[]): Element {
+        const wrapper = new BaseComponent({ tag: "div", classNames }).getElement();
         this.appendElement(wrapper);
         return wrapper;
+    }
+
+    private createForm(formClass: string): HTMLFormElement {
+        const form = new BaseComponent<HTMLFormElement>({
+            tag: "form",
+            classNames: ["main__form", formClass],
+        }).getElement();
+        return form;
     }
 
     private createInput(classNames: string[], type: InputTYPES): HTMLInputElement {
@@ -37,11 +72,11 @@ export class Main extends BaseComponent {
         return input;
     }
 
-    private createBtn(): Element {
+    private createBtn(btnClass: string[], text: string): Element {
         const btn = new BaseComponent({
             tag: "button",
-            classNames: ["btn", "btn__main", "btn-create"],
-            text: "Create",
+            classNames: btnClass,
+            text: text,
         }).getElement();
         return btn;
     }
